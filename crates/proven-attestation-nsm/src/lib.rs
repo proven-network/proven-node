da9cecdbcf12caa6d7527f6c9d34bcf66ad10719
flightofthefox <foxy@borderlinelabs.com>
2024-06-19 18:04:27 +0800

    Refactor nsm.

--- crates/proven-attestation-nsm/src/lib.rs
+++ crates/proven-attestation-nsm/src/lib.rs
@@ -1,36 +1,23 @@
 mod error;
+mod nsm;
 
 pub use error::{Error, Result};
 
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use async_trait::async_trait;
-use aws_nitro_enclaves_nsm_api::api::{Request, Response};
-use aws_nitro_enclaves_nsm_api::driver::{nsm_exit, nsm_init, nsm_process_request};
 use proven_attestation::{AttestationParams, Attestor};
-use serde_bytes::ByteBuf;
-use tracing::error;
+use tokio::sync::Mutex;
 
 #[derive(Debug)]
 pub struct NsmAttestor {
-    fd: Arc<Mutex<i32>>,
+    nsm: Arc<Mutex<nsm::Nsm>>,
 }
 
 impl NsmAttestor {
     pub fn new() -> Self {
         Self {
-            fd: Arc::new(Mutex::new(nsm_init())),
-        }
-    }
-
-    fn process_request(&self, req: Request) -> Result<Response> {
-        let fd = self.fd.lock().unwrap();
-        match nsm_process_request(*fd, req) {
-            Response::Error(err) => {
-                error!("Request error: {:?}", err);
-                Err(Error::RequestError(err))
-            }
-            resp => Ok(resp),
+            nsm: Arc::new(Mutex::new(nsm::Nsm::new())),
         }
     }
 }
@@ -40,30 +27,18 @@ impl Attestor for NsmAttestor {
     type AE = Error;
 
     async fn attest(&self, params: AttestationParams) -> Result<Vec<u8>> {
-        let req = Request::Attestation {
-            nonce: params.nonce.map(ByteBuf::from),
-            public_key: params.public_key.map(ByteBuf::from),
-            user_data: params.user_data.map(ByteBuf::from),
-        };
-
-        match self.process_request(req)? {
-            Response::Attestation { document } => Ok(document),
-            _ => Err(Error::UnexpectedResponse),
-        }
+        self.nsm.lock().await.attestation(params)
     }
 
     async fn secure_random(&self) -> Result<Vec<u8>> {
-        match self.process_request(Request::GetRandom {})? {
-            Response::GetRandom { random } => Ok(random),
-            _ => Err(Error::UnexpectedResponse),
-        }
+        self.nsm.lock().await.get_random()
     }
 }
 
 impl Clone for NsmAttestor {
     fn clone(&self) -> Self {
         Self {
-            fd: Arc::clone(&self.fd),
+            nsm: Arc::clone(&self.nsm),
         }
     }
 }
@@ -71,12 +46,5 @@ impl Clone for NsmAttestor {
 impl Default for NsmAttestor {
     fn default() -> Self {
         Self::new()
-    }
-}
-
-impl Drop for NsmAttestor {
-    fn drop(&mut self) {
-        let fd = self.fd.lock().unwrap();
-        nsm_exit(*fd);
     }
 }
---
+++ crates/proven-attestation-nsm/src/nsm.rs
@@ -0,0 +1,51 @@
+use crate::{Error, Result};
+
+use aws_nitro_enclaves_nsm_api::api::{Request, Response};
+use aws_nitro_enclaves_nsm_api::driver::{nsm_exit, nsm_init, nsm_process_request};
+use proven_attestation::AttestationParams;
+use serde_bytes::ByteBuf;
+
+#[derive(Debug)]
+pub struct Nsm {
+    fd: i32,
+}
+
+impl Nsm {
+    pub fn new() -> Self {
+        Self { fd: nsm_init() }
+    }
+
+    pub fn get_random(&self) -> Result<Vec<u8>> {
+        match self.process_request(Request::GetRandom {})? {
+            Response::GetRandom { random } => Ok(random),
+
+            _ => Err(Error::UnexpectedResponse),
+        }
+    }
+
+    pub fn attestation(&self, params: AttestationParams) -> Result<Vec<u8>> {
+        let req = Request::Attestation {
+            nonce: params.nonce.map(ByteBuf::from),
+            user_data: params.user_data.map(ByteBuf::from),
+            public_key: params.public_key.map(ByteBuf::from),
+        };
+
+        match self.process_request(req)? {
+            Response::Attestation { document } => Ok(document),
+            _ => Err(Error::UnexpectedResponse),
+        }
+    }
+
+    fn process_request(&self, req: Request) -> Result<Response> {
+        match nsm_process_request(self.fd, req) {
+            Response::Error(err) => Err(Error::RequestError(err)),
+            resp => Ok(resp),
+        }
+    }
+}
+
+impl Drop for Nsm {
+    fn drop(&mut self) {
+        nsm_exit(self.fd);
+    }
+}
