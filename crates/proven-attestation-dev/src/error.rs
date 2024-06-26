ba1eec269c14baaf70adfeafaeaa03373716ea3b
flightofthefox <foxy@borderlinelabs.com>
2024-06-27 01:14:07 +0800

    Initial mocked nsm.

--- crates/proven-attestation-dev/src/error.rs
+++ crates/proven-attestation-dev/src/error.rs
@@ -1,14 +1,21 @@
+use derive_more::From;
+
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Clone, Debug)]
+#[derive(Debug, From)]
 pub enum Error {
-    Cbor,
+    #[from]
+    Cbor(serde_cbor::Error),
+
+    #[from]
+    Cose(coset::CoseError),
 }
 
 impl core::fmt::Display for Error {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         match self {
-            Error::Cbor => write!(f, "cbor error"),
+            Error::Cbor(e) => write!(f, "CBOR error: {}", e),
+            Error::Cose(e) => write!(f, "COSE error: {}", e),
         }
     }
 }
--- crates/proven-attestation-dev/src/lib.rs
+++ crates/proven-attestation-dev/src/lib.rs
@@ -1,10 +1,14 @@
 mod error;
 
+use coset::CborSerializable;
 use error::{Error, Result};
+
+use std::collections::BTreeMap;
 
 use async_trait::async_trait;
 use proven_attestation::{AttestationParams, Attestor};
 use rand::RngCore;
+use serde::Serialize;
 
 #[derive(Clone, Debug, Default)]
 pub struct DevAttestor {}
@@ -15,13 +19,43 @@ impl DevAttestor {
     }
 }
 
+#[derive(Clone, Debug, Default, Serialize)]
+struct Attestation {
+    pcrs: BTreeMap<u8, Vec<u8>>,
+    nonce: Vec<u8>,
+    user_data: Vec<u8>,
+    public_key: Vec<u8>,
+}
+
+
 #[async_trait]
 impl Attestor for DevAttestor {
     type AE = Error;
 
-    async fn attest(&self, _params: AttestationParams) -> Result<Vec<u8>> {
-        let attestation = vec![0; 0];
-        Ok(attestation)
+    async fn attest(&self, params: AttestationParams) -> Result<Vec<u8>> {
+        // use zerod pcrs in dev mode
+        let mut pcrs = BTreeMap::new();
+        for i in 0..32 {
+            pcrs.insert(i, vec![0; 32]);
+        }
+
+        let attestation = Attestation {
+            pcrs,
+            nonce: params.nonce.unwrap_or_default(),
+            user_data: params.user_data.unwrap_or_default(),
+            public_key: params.public_key.unwrap_or_default(),
+        };
+
+        let payload = serde_cbor::to_vec(&attestation)?;
+
+        let sign1 = coset::CoseSign1Builder::new()
+            .payload(payload)
+            .create_signature(b"", |_| vec![0; 64])
+            .build();
+
+        let attestation_document = sign1.to_vec()?;
+
+        Ok(attestation_document)
     }
 
     async fn secure_random(&self) -> Result<Vec<u8>> {
