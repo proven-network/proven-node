6c54f10e99d292433012a2a5412b477eb94f69a9
flightofthefox <foxy@borderlinelabs.com>
2024-09-13 05:26:24 +0800

    Increase resources.

--- crates/proven-host/src/main.rs
+++ crates/proven-host/src/main.rs
@@ -43,13 +43,13 @@ struct Args {
     #[arg(long, default_value_t = 4)]
     enclave_cid: u32,
 
-    #[arg(long, default_value_t = 6)]
+    #[arg(long, default_value_t = 13)]
     enclave_cpus: u8,
 
     #[arg(long, default_value_t = Ipv4Addr::new(10, 0, 0, 2))]
     enclave_ip: Ipv4Addr,
 
-    #[arg(long, default_value_t = 8192)]
+    #[arg(long, default_value_t = 28672)]
     enclave_memory: u32,
 
     #[clap(long, required = true)]
