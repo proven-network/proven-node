7a7e81347f37ff9cb9fc4c05b5115ec8ad525faa
flightofthefox <foxy@borderlinelabs.com>
2024-09-13 05:59:25 +0800

    Give small memory buffer.

--- crates/proven-host/src/main.rs
+++ crates/proven-host/src/main.rs
@@ -243,7 +243,8 @@ cpu_count: {}
 # Example of reserving CPUs 2, 3, and 6 through 9:
 # cpu_pool: 2,3,6-9
     "#,
-        enclave_memory, enclave_cpus
+        enclave_memory + 10,
+        enclave_cpus
     );
 
     std::fs::write("/etc/nitro_enclaves/allocator.yaml", allocator_config)?;
