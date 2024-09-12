9c5ce12c62bfbd3c14dfcb826c445cfa099cdd76
flightofthefox <foxy@borderlinelabs.com>
2024-09-13 06:39:28 +0800

    Tweak nfs.

--- crates/proven-external-fs/src/lib.rs
+++ crates/proven-external-fs/src/lib.rs
@@ -163,7 +163,7 @@ impl ExternalFs {
             .arg("-t")
             .arg("nfs")
             .arg("-o")
-            .arg("noatime,nfsvers=4.2,sync,rsize=1048576,wsize=1048576")
+            .arg("noatime,noacl,nocto,nfsvers=4.2,sync,nconnect=16,rsize=1048576,wsize=1048576")
             .arg(self.nfs_server.as_str())
             .arg(NFS_DIR)
             .stdout(Stdio::inherit())
