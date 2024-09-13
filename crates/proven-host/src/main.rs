353b61beb8cede5ddf4a2ec469195f25c9935a02
flightofthefox <foxy@borderlinelabs.com>
2024-09-13 11:52:59 +0800

    Update memory.

--- crates/proven-host/src/main.rs
+++ crates/proven-host/src/main.rs
@@ -49,7 +49,7 @@ struct Args {
     #[arg(long, default_value_t = Ipv4Addr::new(10, 0, 0, 2))]
     enclave_ip: Ipv4Addr,
 
-    #[arg(long, default_value_t = 26624)]
+    #[arg(long, default_value_t = 25000)]
     enclave_memory: u32,
 
     #[clap(long, required = true)]
