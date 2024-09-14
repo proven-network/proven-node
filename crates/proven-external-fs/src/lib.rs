1877c1e0b6fce502f336105a2e66108b37034089
flightofthefox <foxy@borderlinelabs.com>
2024-09-15 06:44:03 +0800

    Change lookup cache.

--- crates/proven-external-fs/src/lib.rs
+++ crates/proven-external-fs/src/lib.rs
@@ -240,7 +240,9 @@ async fn mount_nfs(nfs_mount_point: String, nfs_mount_dir: String) -> Result<()>
         .arg("-t")
         .arg("nfs")
         .arg("-o")
-        .arg("noac,noatime,nolock,nfsvers=3,sync,nconnect=16,rsize=1048576,wsize=1048576")
+        .arg(
+            "noatime,nolock,nfsvers=3,sync,nconnect=16,rsize=1048576,wsize=1048576,lookupcache=pos",
+        )
         .arg(nfs_mount_point.as_str())
         .arg(nfs_mount_dir.as_str())
         .stdout(Stdio::inherit())
