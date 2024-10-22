6058debbf87b3b1f5a1b11c5bca69b47d9531d0e
flightofthefox <foxy@borderlinelabs.com>
2024-10-22 07:18:26 +0700

    Store docs.

--- crates/proven-store/src/lib.rs
+++ crates/proven-store/src/lib.rs
@@ -3,6 +3,15 @@ use std::fmt::Debug;
 
 use async_trait::async_trait;
 
+/// A trait representing a key-value store with asynchronous operations.
+///
+/// # Associated Types
+/// - `SE`: The error type that implements `Debug`, `Error`, `Send`, and `Sync`.
+///
+/// # Required Methods
+/// - `async fn del(&self, key: String) -> Result<(), Self::SE>`: Deletes a key from the store.
+/// - `async fn get(&self, key: String) -> Result<Option<Vec<u8>>, Self::SE>`: Retrieves the value associated with a key.
+/// - `async fn put(&self, key: String, bytes: Vec<u8>) -> Result<(), Self::SE>`: Stores a key-value pair.
 #[async_trait]
 pub trait Store: Clone + Send + Sync + 'static {
     type SE: Debug + Error + Send + Sync;
@@ -13,6 +22,14 @@ pub trait Store: Clone + Send + Sync + 'static {
 }
 
 #[async_trait]
+/// A trait representing a scoped key-value store with asynchronous operations.
+///
+/// # Associated Types
+/// - `SE`: The error type that implements `Debug`, `Error`, `Send`, and `Sync`.
+/// - `Scoped`: The scoped store type that implements the `Store` trait.
+///
+/// # Required Methods
+/// - `fn scope(&self, scope: String) -> Self::Scoped`: Add a scope and make the store usable.
 pub trait Store1: Clone + Send + Sync + 'static {
     type SE: Debug + Error + Send + Sync;
     type Scoped: Store<SE = Self::SE>;
@@ -20,6 +37,14 @@ pub trait Store1: Clone + Send + Sync + 'static {
     fn scope(&self, scope: String) -> Self::Scoped;
 }
 
+/// A trait representing a double-scoped key-value store with asynchronous operations.
+///
+/// # Associated Types
+/// - `SE`: The error type that implements `Debug`, `Error`, `Send`, and `Sync`.
+/// - `Scoped`: The double-scoped store type that implements the `Store1` trait.
+///
+/// # Required Methods
+/// - `fn scope(&self, scope: String) -> Self::Scoped`: Add one of two scopee.
 #[async_trait]
 pub trait Store2: Clone + Send + Sync + 'static {
     type SE: Debug + Error + Send + Sync;
