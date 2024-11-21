c4d3037b74f991e81d7aca22af6e3fb593141fd8
flightofthefox <foxy@borderlinelabs.com>
2024-11-21 21:33:34 +0800

    Update sessions to thiserror.

--- crates/sessions/src/error.rs
+++ crates/sessions/src/error.rs
@@ -1,14 +1,27 @@
 use std::sync::Arc;
 
+use thiserror::Error;
+
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Error)]
 pub enum Error {
+    #[error("attestation error")]
     Attestation,
+
+    #[error(transparent)]
     CborSerialize(Arc<ciborium::ser::Error<std::io::Error>>),
+
+    #[error(transparent)]
     CborDeserialize(Arc<ciborium::de::Error<std::io::Error>>),
+
+    #[error("challenge store error")]
     ChallengeStore,
+
+    #[error("session store error")]
     SessionStore,
+
+    #[error("signed challenge is invalid")]
     SignedChallengeInvalid,
 }
 
