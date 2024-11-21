ff964ab748fcff23de705b927f6e7dfa36ac3516
flightofthefox <foxy@borderlinelabs.com>
2024-11-21 19:27:42 +0800

    Update external-fs error.

--- crates/external-fs/src/error.rs
+++ crates/external-fs/src/error.rs
@@ -1,26 +1,18 @@
-use derive_more::From;
+use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug, From)]
+#[derive(Debug, Error)]
 pub enum Error {
+    #[error("external fs already started")]
     AlreadyStarted,
-    OutputParse,
+
+    #[error("external fs exited with non-zero: {0}")]
     NonZeroExitCode(std::process::ExitStatus),
-    Spawn(std::io::Error),
-}
 
-impl core::fmt::Display for Error {
-    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        match self {
-            Error::AlreadyStarted => write!(f, "external fs already started"),
-            Error::NonZeroExitCode(status) => {
-                write!(f, "external fs exited with non-zero: {}", status)
-            }
-            Error::OutputParse => write!(f, "failed to parse external fs output"),
-            Error::Spawn(e) => write!(f, "failed to spawn: {}", e),
-        }
-    }
-}
+    #[error("failed to parse external fs output")]
+    OutputParse,
 
-impl std::error::Error for Error {}
+    #[error("failed to spawn: {0}")]
+    Spawn(#[from] std::io::Error),
+}
