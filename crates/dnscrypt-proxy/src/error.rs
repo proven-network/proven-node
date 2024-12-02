a79323f75d6becb0ee89df7546bb47980aa59a81
flightofthefox <foxy@borderlinelabs.com>
2024-12-02 09:50:29 +0800

    Document dnscrypt-proxy.

--- crates/dnscrypt-proxy/src/error.rs
+++ crates/dnscrypt-proxy/src/error.rs
@@ -1,27 +1,52 @@
 use thiserror::Error;
 
+/// The result type for this crate.
 pub type Result<T> = std::result::Result<T, Error>;
 
+/// Errors that can occur in this crate.
 #[derive(Debug, Error)]
 pub enum Error {
+    /// Failed to parse socket address.
+    #[error(transparent)]
+    AddrParse(#[from] std::net::AddrParseError),
+
+    /// Proxy already started.
     #[error("dnscrypt-proxy already started")]
     AlreadyStarted,
 
+    /// Failed to write config file.
     #[error("failed to write nats config: {0}")]
     ConfigWrite(#[from] std::io::Error),
 
+    /// Failed to encode DNS stamp.
+    #[error(transparent)]
+    DnsStampEncode(#[from] dns_stamp_parser::EncodeError),
+
+    /// Failed to parse dnscrypt-proxy output.
     #[error("failed to parse dnscrypt-proxy output")]
     OutputParse,
 
+    /// Process exited with non-zero.
     #[error("dnscrypt-proxy exited with non-zero: {0}")]
     NonZeroExitCode(std::process::ExitStatus),
 
+    /// Failed to parse regex pattern.
+    #[error(transparent)]
+    RegexParse(#[from] regex::Error),
+
+    /// Failed to find local DOH resolver endpoint in Route53.
     #[error("failed to find local DoH resolver endpoint in Route53")]
     ResolverEndpointNotFound,
 
-    #[error("{0}")]
+    /// Failed to resolve Route53 resolver endpoint.
+    #[error(transparent)]
     Route53(#[from] aws_sdk_route53resolver::Error),
 
-    #[error("failed to spawn dnscrypt-proxy: {0}")]
+    /// Failed to send signal to dnscrypt-proxy.
+    #[error(transparent)]
+    Signal(#[from] nix::Error),
+
+    /// Failed to spawn dnscrypt-proxy.
+    #[error(transparent)]
     Spawn(std::io::Error),
 }
--- crates/dnscrypt-proxy/src/lib.rs
+++ crates/dnscrypt-proxy/src/lib.rs
@@ -1,3 +1,8 @@
+//! Configures and runs a DNS Crypt proxy to ensure all DNS runs over tamper-proof HTTPS.
+#![warn(missing_docs)]
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+
 mod error;
 
 use aws_sdk_route53resolver::types::IpAddressStatus;
@@ -17,6 +22,7 @@ use tokio_util::sync::CancellationToken;
 use tokio_util::task::TaskTracker;
 use tracing::{debug, error, info, warn};
 
+/// Dnscrypt proxy runner.
 pub struct DnscryptProxy {
     region: String,
     vpc_id: String,
@@ -26,24 +32,31 @@ pub struct DnscryptProxy {
     task_tracker: TaskTracker,
 }
 
+/// Options for creating a new `DnscryptProxy`.
+pub struct DnscryptProxyOptions {
+    /// The current AWS AZ.
+    pub availability_zone: String,
+
+    /// The current AWS region.
+    pub region: String,
+
+    /// The current subnet ID.
+    pub subnet_id: String,
+
+    /// The current VPC ID.
+    pub vpc_id: String,
+}
+
 impl DnscryptProxy {
     /// Creates a new instance of `DnscryptProxy`.
-    ///
-    /// # Arguments
-    ///
-    /// * `region` - The AWS region.
-    /// * `vpc_id` - The VPC ID.
-    /// * `availability_zone` - The availability zone.
-    /// * `subnet_id` - The subnet ID.
-    ///
-    /// # Returns
-    ///
-    /// A new instance of `DnscryptProxy`.
+    #[must_use]
     pub fn new(
-        region: String,
-        vpc_id: String,
-        availability_zone: String,
-        subnet_id: String,
+        DnscryptProxyOptions {
+            availability_zone,
+            region,
+            subnet_id,
+            vpc_id,
+        }: DnscryptProxyOptions,
     ) -> Self {
         Self {
             region,
@@ -57,20 +70,164 @@ impl DnscryptProxy {
 
     /// Starts the proxy server.
     ///
-    /// # Returns
+    /// # Errors
     ///
-    /// A `JoinHandle` that can be used to await the completion of the server task.
+    /// This function will return an error if the proxy server is already started,
+    /// if there is an issue spawning the dnscrypt-proxy process, or if there is
+    /// an issue parsing the output or handling signals.
     pub async fn start(&self) -> Result<JoinHandle<Result<()>>> {
         if self.task_tracker.is_closed() {
             return Err(Error::AlreadyStarted);
         }
 
+        let doh_ip = self.get_doh_ip().await?;
+        self.update_dnscrypt_config(doh_ip).await?;
+
+        let shutdown_token = self.shutdown_token.clone();
+        let task_tracker = self.task_tracker.clone();
+
+        let server_task = self.task_tracker.spawn(async move {
+            // Start the dnscrypt-proxy process
+            let mut cmd = Command::new("dnscrypt-proxy")
+                .arg("-config")
+                .arg("/etc/dnscrypt-proxy/dnscrypt-proxy.toml")
+                .stdout(Stdio::null())
+                .stderr(Stdio::piped())
+                .spawn()
+                .map_err(Error::Spawn)?;
+
+            let stderr = cmd.stderr.take().ok_or(Error::OutputParse)?;
+
+            // Spawn a task to read and process the stderr output of the dnscrypt-proxy process
+            task_tracker.spawn(async move {
+                let reader = BufReader::new(stderr);
+                let mut lines = reader.lines();
+
+                let re = Regex::new(r"(\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\]) (\[[A-Z]+\]) (.*)")
+                    .map_err(Error::RegexParse)?;
+
+                while let Ok(Some(line)) = lines.next_line().await {
+                    if let Some(caps) = re.captures(&line) {
+                        let label = caps.get(2).map_or("[UNKNOWN]", |m| m.as_str());
+                        let message = caps.get(3).map_or(line.as_str(), |m| m.as_str());
+                        match label {
+                            "[INFO]" => info!("{}", message),
+                            "[NOTICE]" => info!("{}", message),
+                            "[DEBUG]" => debug!("{}", message),
+                            "[WARNING]" => warn!("{}", message),
+                            "[CRITICAL]" => warn!("{}", message),
+                            "[ERROR]" => error!("{}", message),
+                            "[FATAL]" => error!("{}", message),
+                            _ => error!("{}", line),
+                        }
+                    } else {
+                        error!("{}", line);
+                    }
+                }
+
+                Ok::<(), Error>(())
+            });
+
+            // Wait for the dnscrypt-proxy process to exit or for the shutdown token to be cancelled
+            tokio::select! {
+                result = cmd.wait() => {
+                    let status = result.map_err(Error::Spawn)?;
+
+                    if !status.success() {
+                        return Err(Error::NonZeroExitCode(status));
+                    }
+
+                    Ok(())
+                }
+                () = shutdown_token.cancelled() => {
+                    #[allow(clippy::cast_possible_wrap)]
+                    let pid = Pid::from_raw(cmd.id().ok_or(Error::OutputParse)? as i32);
+                    signal::kill(pid, Signal::SIGTERM).map_err(Error::Signal)?;
+
+                    let _ = cmd.wait().await;
+
+                    Ok(())
+                }
+            }
+        });
+
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+        self.task_tracker.close();
+
+        Ok(server_task)
+    }
+
+    /// Shuts down the server.
+    pub async fn shutdown(&self) {
+        info!("dnscrypt-proxy shutting down...");
+
+        self.shutdown_token.cancel();
+        self.task_tracker.wait().await;
+
+        info!("dnscrypt-proxy shutdown");
+    }
+
+    /// Updates the dnscrypt-proxy configuration.
+    ///
+    /// # Arguments
+    ///
+    /// * `doh_ip` - The `IPv4Addr` of the DOH resolver.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` indicating success or failure.
+    async fn update_dnscrypt_config(&self, doh_ip: Ipv4Addr) -> Result<()> {
+        use dns_stamp_parser::{Addr, DnsOverHttps, DnsStamp, Props};
+
+        let props = Props::DNSSEC;
+        let sock_addr = SocketAddrV4::new(doh_ip, 443);
+        let addr = Some(Addr::SocketAddr(
+            sock_addr.to_string().parse().map_err(Error::AddrParse)?,
+        ));
+        let hostname = format!("route53resolver.{}.amazonaws.com", self.availability_zone);
+        let path = "/dns-query".to_string();
+        let dns_stamp = DnsStamp::DnsOverHttps(DnsOverHttps {
+            props,
+            addr,
+            hashi: Vec::new(),
+            hostname,
+            path,
+            bootstrap_ipi: Vec::new(),
+        });
+
+        let config = format!(
+            r"
+# dnscrypt-proxy configuration file
+
+# Listen addresses
+listen_addresses = ['127.0.0.1:53', '[::1]:53']
+
+# Server names
+server_names = ['route53-resolver']
+
+# Static server definitions
+[static.'route53-resolver']
+stamp = '{}'
+        ",
+            dns_stamp.encode()?
+        );
+
+        tokio::fs::create_dir_all("/etc/dnscrypt-proxy")
+            .await
+            .map_err(Error::ConfigWrite)?;
+
+        tokio::fs::write("/etc/dnscrypt-proxy/dnscrypt-proxy.toml", config)
+            .await
+            .map_err(Error::ConfigWrite)
+    }
+
+    async fn get_doh_ip(&self) -> Result<Ipv4Addr> {
         let config = aws_config::from_env()
             .region(Region::new(self.region.clone()))
             .load()
             .await;
 
-        // get inbound route53 resolver by vpc
         let vpc_filter = aws_sdk_route53resolver::types::Filter::builder()
             .name("HostVPCId")
             .values(self.vpc_id.clone())
@@ -83,6 +240,7 @@ impl DnscryptProxy {
             .name("Status")
             .values("OPERATIONAL".to_string())
             .build();
+
         let route53_client = aws_sdk_route53resolver::Client::new(&config);
         let resolver_endpoints = route53_client
             .list_resolver_endpoints()
@@ -128,139 +286,6 @@ impl DnscryptProxy {
             .parse::<Ipv4Addr>()
             .map_err(|_| Error::ResolverEndpointNotFound)?;
 
-        self.update_dnscrypt_config(doh_ip).await?;
-
-        let shutdown_token = self.shutdown_token.clone();
-        let task_tracker = self.task_tracker.clone();
-
-        let server_task = self.task_tracker.spawn(async move {
-            // Start the dnscrypt-proxy process
-            let mut cmd = Command::new("dnscrypt-proxy")
-                .arg("-config")
-                .arg("/etc/dnscrypt-proxy/dnscrypt-proxy.toml")
-                .stdout(Stdio::null())
-                .stderr(Stdio::piped())
-                .spawn()
-                .map_err(Error::Spawn)?;
-
-            let stderr = cmd.stderr.take().ok_or(Error::OutputParse)?;
-
-            // Spawn a task to read and process the stderr output of the dnscrypt-proxy process
-            task_tracker.spawn(async move {
-                let reader = BufReader::new(stderr);
-                let mut lines = reader.lines();
-
-                let re = Regex::new(r"(\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\]) (\[[A-Z]+\]) (.*)")
-                    .unwrap();
-
-                while let Ok(Some(line)) = lines.next_line().await {
-                    if let Some(caps) = re.captures(&line) {
-                        let label = caps.get(2).unwrap().as_str();
-                        let message = caps.get(3).unwrap().as_str();
-                        match label {
-                            "[INFO]" => info!("{}", message),
-                            "[NOTICE]" => info!("{}", message),
-                            "[DEBUG]" => debug!("{}", message),
-                            "[WARNING]" => warn!("{}", message),
-                            "[CRITICAL]" => warn!("{}", message),
-                            "[ERROR]" => error!("{}", message),
-                            "[FATAL]" => error!("{}", message),
-                            _ => error!("{}", line),
-                        }
-                    } else {
-                        error!("{}", line);
-                    }
-                }
-            });
-
-            // Wait for the dnscrypt-proxy process to exit or for the shutdown token to be cancelled
-            tokio::select! {
-                _ = cmd.wait() => {
-                    let status = cmd.wait().await.unwrap();
-
-                    if !status.success() {
-                        return Err(Error::NonZeroExitCode(status));
-                    }
-
-                    Ok(())
-                }
-                _ = shutdown_token.cancelled() => {
-                    let pid = Pid::from_raw(cmd.id().unwrap() as i32);
-                    signal::kill(pid, Signal::SIGTERM).unwrap();
-
-                    let _ = cmd.wait().await;
-
-                    Ok(())
-                }
-            }
-        });
-
-        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-
-        self.task_tracker.close();
-
-        Ok(server_task)
-    }
-
-    /// Shuts down the server.
-    pub async fn shutdown(&self) {
-        info!("dnscrypt-proxy shutting down...");
-
-        self.shutdown_token.cancel();
-        self.task_tracker.wait().await;
-
-        info!("dnscrypt-proxy shutdown");
-    }
-
-    /// Updates the dnscrypt-proxy configuration.
-    ///
-    /// # Arguments
-    ///
-    /// * `doh_ip` - The IPv4Addr of the DoH resolver.
-    ///
-    /// # Returns
-    ///
-    /// A `Result` indicating success or failure.
-    async fn update_dnscrypt_config(&self, doh_ip: Ipv4Addr) -> Result<()> {
-        use dns_stamp_parser::{Addr, DnsOverHttps, DnsStamp, Props};
-
-        let props = Props::DNSSEC;
-        let sock_addr = SocketAddrV4::new(doh_ip, 443);
-        let addr = Some(Addr::SocketAddr(sock_addr.to_string().parse().unwrap()));
-        let hostname = format!("route53resolver.{}.amazonaws.com", self.availability_zone);
-        let path = "/dns-query".to_string();
-        let dns_stamp = DnsStamp::DnsOverHttps(DnsOverHttps {
-            props,
-            addr,
-            hashi: Vec::new(),
-            hostname,
-            path,
-            bootstrap_ipi: Vec::new(),
-        });
-
-        let config = format!(
-            r#"
-# dnscrypt-proxy configuration file
-
-# Listen addresses
-listen_addresses = ['127.0.0.1:53', '[::1]:53']
-
-# Server names
-server_names = ['route53-resolver']
-
-# Static server definitions
-[static.'route53-resolver']
-stamp = '{}'
-        "#,
-            dns_stamp.encode().unwrap()
-        );
-
-        tokio::fs::create_dir_all("/etc/dnscrypt-proxy")
-            .await
-            .unwrap();
-
-        tokio::fs::write("/etc/dnscrypt-proxy/dnscrypt-proxy.toml", config)
-            .await
-            .map_err(Error::ConfigWrite)
+        Ok(doh_ip)
     }
 }
