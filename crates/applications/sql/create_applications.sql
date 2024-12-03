71e77b4589cd927a1afd4ebb0f10a04cd5b26d16
flightofthefox <foxy@borderlinelabs.com>
2024-12-04 06:40:14 +0800

    Fix SQL error.

--- crates/applications/sql/create_applications.sql
+++ crates/applications/sql/create_applications.sql
@@ -2,5 +2,5 @@ CREATE TABLE applications (
     id TEXT PRIMARY KEY,
     owner_identity TEXT NOT NULL,
     created_at INTEGER NOT NULL,
-    updated_at INTEGER NOT NULL,
+    updated_at INTEGER NOT NULL
 );
