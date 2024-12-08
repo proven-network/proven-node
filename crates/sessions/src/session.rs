23761b1e558402d48e528adb41d146b375fa04b2
flightofthefox <foxy@borderlinelabs.com>
2024-12-08 11:00:04 +0800

    Impl session serde.

--- crates/sessions/src/session.rs
+++ crates/sessions/src/session.rs
@@ -1,3 +1,4 @@
+use bytes::Bytes;
 use serde::{Deserialize, Serialize};
 
 /// A session.
@@ -23,4 +24,23 @@ pub struct Session {
 
     /// The node verifying key.
     pub verifying_key: Vec<u8>,
+}
+
+impl TryFrom<Bytes> for Session {
+    type Error = ciborium::de::Error<std::io::Error>;
+
+    fn try_from(bytes: Bytes) -> Result<Self, Self::Error> {
+        let reader = bytes.as_ref();
+        ciborium::de::from_reader(reader)
+    }
+}
+
+impl TryInto<Bytes> for Session {
+    type Error = ciborium::ser::Error<std::io::Error>;
+
+    fn try_into(self) -> Result<Bytes, Self::Error> {
+        let mut writer = Vec::new();
+        ciborium::ser::into_writer(&self, &mut writer)?;
+        Ok(Bytes::from(writer))
+    }
 }
