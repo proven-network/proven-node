b01ab71c8eaf02b4762d7537b921f7e6017025a0
flightofthefox <foxy@borderlinelabs.com>
2024-12-12 13:42:59 +0800

    Start nats service impl.

--- crates/messaging-nats/src/service/mod.rs
+++ crates/messaging-nats/src/service/mod.rs
@@ -5,21 +5,29 @@ pub use error::Error;
 
 use std::error::Error as StdError;
 use std::fmt::Debug;
-use std::marker::PhantomData;
-use std::sync::Arc;
 
+use async_nats::jetstream::consumer::pull::Config as NatsConsumerConfig;
+use async_nats::jetstream::consumer::Consumer as NatsConsumerType;
+use async_nats::jetstream::Context;
+use async_nats::Client as NatsClient;
 use async_trait::async_trait;
 use bytes::Bytes;
+use futures::StreamExt;
 use proven_messaging::service::{Service, ServiceOptions};
 use proven_messaging::service_handler::ServiceHandler;
-use tokio::sync::Mutex;
+use proven_messaging::stream::InitializedStream;
+use proven_messaging::Message;
 
-/// Options for the in-memory subscriber (there are none).
+/// Options for the nats service.
 #[derive(Clone, Debug)]
-pub struct NatsServiceOptions;
+pub struct NatsServiceOptions {
+    durable_name: Option<String>,
+    nats_client: NatsClient,
+    nats_jetstream_context: Context,
+}
 impl ServiceOptions for NatsServiceOptions {}
 
-/// A in-memory subscriber.
+/// A NATS service.
 #[derive(Debug)]
 pub struct NatsService<X, T, D, S>
 where
@@ -34,9 +42,10 @@ where
     D: Debug + Send + StdError + Sync + 'static,
     S: Debug + Send + StdError + Sync + 'static,
 {
-    last_seq: Arc<Mutex<u64>>,
+    nats_client: NatsClient,
+    nats_consumer: NatsConsumerType<NatsConsumerConfig>,
+    nats_jetstream_context: Context,
     stream: <Self as Service<X, T, D, S>>::StreamType,
-    _marker: PhantomData<T>,
 }
 
 impl<X, T, D, S> Clone for NatsService<X, T, D, S>
@@ -54,9 +63,49 @@ where
 {
     fn clone(&self) -> Self {
         Self {
-            last_seq: self.last_seq.clone(),
+            nats_client: self.nats_client.clone(),
+            nats_consumer: self.nats_consumer.clone(),
+            nats_jetstream_context: self.nats_jetstream_context.clone(),
             stream: self.stream.clone(),
-            _marker: PhantomData,
+        }
+    }
+}
+
+impl<X, T, D, S> NatsService<X, T, D, S>
+where
+    X: ServiceHandler<T, D, S> + Clone + Debug + Send + Sync + 'static,
+    T: Clone
+        + Debug
+        + Send
+        + Sync
+        + TryFrom<Bytes, Error = D>
+        + TryInto<Bytes, Error = S>
+        + 'static,
+    D: Debug + Send + StdError + Sync + 'static,
+    S: Debug + Send + StdError + Sync + 'static,
+{
+    /// Creates a new NATS consumer.
+    async fn process_messages(
+        nats_consumer: NatsConsumerType<NatsConsumerConfig>,
+        handler: X,
+    ) -> Result<(), Error> {
+        loop {
+            let mut messages = nats_consumer
+                .messages()
+                .await
+                .map_err(|e| Error::Stream(e.kind()))?;
+
+            while let Some(message) = messages.next().await {
+                let message = message.map_err(|e| Error::Messages(e.kind()))?;
+
+                let headers = message.headers.clone();
+                let payload: T = message.payload.clone().try_into().unwrap();
+
+                handler
+                    .handle(Message { headers, payload })
+                    .await
+                    .map_err(|_| Error::Handler)?;
+            }
         }
     }
 }
@@ -82,23 +131,48 @@ where
     type StreamType = InitializedNatsStream<T, D, S>;
 
     async fn new(
-        _name: String,
+        name: String,
         stream: Self::StreamType,
-        _options: NatsServiceOptions,
-        _handler: X,
+        options: NatsServiceOptions,
+        handler: X,
     ) -> Result<Self, Self::Error> {
-        let last_seq = Arc::new(Mutex::new(0));
+        let nats_consumer = options
+            .nats_jetstream_context
+            .create_consumer_on_stream(
+                NatsConsumerConfig {
+                    name: Some(name),
+                    durable_name: options.durable_name,
+                    ..Default::default()
+                },
+                stream.name().as_str(),
+            )
+            .await
+            .map_err(|e| Error::Create(e.kind()))?;
+
+        tokio::spawn(Self::process_messages(
+            nats_consumer.clone(),
+            handler.clone(),
+        ));
 
         Ok(Self {
-            last_seq,
+            nats_client: options.nats_client,
+            nats_consumer,
+            nats_jetstream_context: options.nats_jetstream_context,
             stream,
-            _marker: PhantomData,
         })
     }
 
     async fn last_seq(&self) -> Result<u64, Self::Error> {
-        let seq = self.last_seq.lock().await;
-        Ok(*seq)
+        let seq = self
+            .nats_consumer
+            .clone()
+            .info()
+            .await
+            .map_err(|e| Error::Info(e.kind()))?
+            .ack_floor
+            .stream_sequence;
+
+        Ok(seq)
     }
 
     fn stream(&self) -> Self::StreamType {
