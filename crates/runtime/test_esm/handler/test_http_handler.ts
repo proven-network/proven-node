8c650b6af45bfca2b52eb0a459ee858b67ad3959
flightofthefox <foxy@borderlinelabs.com>
2025-01-22 10:45:51 +0800

    Add tests for HTTP request fields.

--- crates/runtime/test_esm/handler/test_http_handler.ts
+++ crates/runtime/test_esm/handler/test_http_handler.ts
@@ -2,6 +2,24 @@ import { runOnHttp } from "@proven-network/handler";
 
 export const test = runOnHttp(
   async (request) => {
+    if (request.path !== "/test/420") {
+      throw new Error("Expected path to be /test/420");
+    }
+
+    if (request.method !== "GET") {
+      throw new Error("Expected method to be GET");
+    }
+
+    if (!request.body) {
+      throw new Error("Expected body to be defined");
+    }
+
+    const expectedBytes = new TextEncoder().encode("Hello, world!");
+    if (!request.body.every((byte, i) => byte === expectedBytes[i])) {
+      throw new Error("Expected body to be 'Hello, world!'");
+    }
+
+    // Check path variable extraction works
     return request.pathVariables.id;
   },
   {
